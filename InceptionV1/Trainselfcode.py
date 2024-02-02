13c803c3dbd03ef9801969dcd9b4f96c69067b3b
HaiHoang <viethoangls0201@gmail.com>
2024-02-02 17:14:30 +0700

    Pasr

---
+++ InceptionV1/Trainselfcode.py
@@ -0,0 +1,20 @@
+import torch
+from torchvision import transforms,datasets
+from torch.utils.data import DataLoader, random_split
+import torch.nn as nn
+import torch.optim as optim
+import Model
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+data_dir = 'E:/ModelDeepfromScratch/Data'
+transform = transforms.Compose([
+    transforms.Resize((224,224)),
+    transforms.ToTensor(),
+  
+])
+dataset = datasets.ImageFolder(data_dir, transform=transform)
+train_size = int(0.7*len(dataset))
+test_size  = len(dataset)-train_size
+train_dataset, val_dataset = random_split(dataset, [train_size, test_size])
+train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True, num_workers=4)
+test_loader  = DataLoader(test_size, batch_size=8, shuffle=False, num_workers=4)
+model = Inceptionv1()
